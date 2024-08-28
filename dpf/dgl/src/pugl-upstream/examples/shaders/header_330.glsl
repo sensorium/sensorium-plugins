fd544c13dc262788a741ac0550daa770bc367e4f
mr.sensorium <faveflave@gmail.com>
2024-08-28 11:40:49 +1000

    new go

--- dpf/dgl/src/pugl-upstream/examples/shaders/header_330.glsl
+++
@@ -1,7 +0,0 @@
-// Copyright 2020 David Robillard <d@drobilla.net>
-// SPDX-License-Identifier: ISC
-
-#version 330 core
-
-#define INTER(qualifiers)
-#define UBO(qualifiers) layout(std140)
--- dpf/dgl/src/pugl-upstream/examples/shaders/header_420.glsl
+++
@@ -1,7 +0,0 @@
-// Copyright 2020 David Robillard <d@drobilla.net>
-// SPDX-License-Identifier: ISC
-
-#version 420 core
-
-#define INTER(qualifiers) layout(qualifiers)
-#define UBO(qualifiers) layout(std140, qualifiers)
